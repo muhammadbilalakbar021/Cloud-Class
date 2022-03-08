bd9f98f57fdedb56213f3b9e60b722c8ebeb4924
Tariq Bashir <kaminasw@gmail.com>
2022-03-08 07:58:20 -0800

    CC - 18 Fixed Contact Page Form

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -10,44 +10,60 @@ import SnackBar from '../utils/snakbar';
   styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent implements OnInit {
-  selectedValue: string = '';
-  checkedValue: string = '';
+  userType: string = '';
+  requestType: string = '';
 
   constructor(
     private http: HttpService,
     public dialog: MatDialog,
     private _snackBar: SnackBar
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
-  selectChangeHandler(event: any) {
-    this.selectedValue = event.target.value;
+  ngOnInit(): void { }
+  selectUserType(event: any) {
+    if (event.target.value == 'Select') {
+      this.userType = "";
+    } else {
+      this.userType = event.target.value;
+    }
   }
 
-  selectEventHandler(event: any) {
-    this.checkedValue = event.target.value;
+  selectRequestType(event: any) {
+    if (event.target.value == 'Select') {
+      this.requestType = "";
+    } else {
+      this.requestType = event.target.value;
+    }
   }
 
-  sendContactData(username: any, name: any, surname: any, details: any) {
+  sendContactData(username: any, name: any, surname: any, details: any, email: any) {
     if (!name) {
       this._snackBar.showSnackBar('Please enter your username', '');
     } else if (!username) {
       this._snackBar.showSnackBar('Please enter your name', '');
     } else if (!surname) {
       this._snackBar.showSnackBar('Please enter your surname', '');
-    } else if (!details) {
-      this._snackBar.showSnackBar('Please enter your details', '');
+    } else if (!email
+      .toLowerCase()
+      .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+      this._snackBar.showSnackBar('Please enter your email', '');
+    } else if (!this.userType) {
+      this._snackBar.showSnackBar('Please select your user type', '');
+    } else if (!this.requestType) {
+      this._snackBar.showSnackBar('Please select your request type', '');
     } else {
       let obj = {
-        usertype: this.selectedValue,
+        usertype: this.userType,
+        requestType: this.requestType,
         message: details,
+        email: email,
         name: name,
         surname: surname,
         username: username,
         password: '',
         tstamp: new Date().toISOString(),
-        actiontaken: 'did something',
-        status: '0',
+        actiontaken: '',
+        status: 0,
       };
       console.log('obj is', obj);
       this.http
